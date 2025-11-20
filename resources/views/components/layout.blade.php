83cc9a1eab22210516f179197cc56f9ceb02e4a5
psyke47 <dmunonde@gmail.com>
2025-11-20 11:30:03 +0200

    Add favicon package and update favicon links in layout and coming-soon components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,11 +30,11 @@
     
 
     {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/x-icon" href="/images/favicon/favicon.ico">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
     <link rel="manifest" href="/images/favicon/site.webmanifest">
-
     {{-- Scrolling Aninmation --}}
     <style>
       @keyframes appear {
